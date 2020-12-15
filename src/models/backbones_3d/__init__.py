0c423bfa859ee470fcdc3d268e9df0ba8f67029c
Prarthana <p6bhatta@uwaterloo.ca>
2020-12-15 07:03:41 -0500

    init file for backbones_3d

---
+++ src/models/backbones_3d/__init__.py
@@ -0,0 +1,15 @@
+from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG, PointNet2MSG_fsa, PointNet2MSG_dsa
+from .spconv_backbone import VoxelBackBone8, SlimVoxelBackBone8x
+from .spconv_unet import UNetV2
+
+__all__ = {
+    'VoxelBackBone8x': VoxelBackBone8x,
+    'VoxelResBackBone8x': VoxelResBackBone8x,
+    'UNetV2': UNetV2,
+    'SlimVoxelBackBone8x': SlimVoxelBackBone8x,
+
+    'PointNet2Backbone': PointNet2Backbone,
+    'PointNet2MSG': PointNet2MSG,
+    'PointNet2MSG_fsa': PointNet2MSG_fsa,
+    'PointNet2MSG_dsa': PointNet2MSG_dsa,
+}
