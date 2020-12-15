55241c5dbb2f944eb76de29a62a73188df3632b2
Prarthana <p6bhatta@uwaterloo.ca>
2020-12-15 11:10:42 -0500

    Add cfe init

--- src/models/backbones_3d/cfe/__init__.py
+++ src/models/backbones_3d/cfe/__init__.py
@@ -6,9 +6,9 @@ from .voxel_dsa import VoxelContext3D_dsa
 
 
 __all__ = {
-    'PillarContext3D': PillarContext3D_fsa,
-    'VoxelContext3D': VoxelContext3D_fsa,
+    'PillarContext3D_fsa': PillarContext3D_fsa,
+    'VoxelContext3D_fsa': VoxelContext3D_fsa,
 
-    'ScalablePillarContext3D': PillarContext3D_dsa,
-    'ScalableVoxelContext3D': VoxelContext3D_dsa,
+    'PillarContext3D_dsa': PillarContext3D_dsa,
+    'VoxelContext3D_dsa': VoxelContext3D_dsa,
 }
