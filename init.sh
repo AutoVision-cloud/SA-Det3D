d38bd018d700c7d03cdf28979fa8d7e1e5c7de8f
Prarthana <p6bhatta@uwaterloo.ca>
2020-12-16 08:43:06 -0500

    Add init file

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# copy files
+cp -r src/models/backbones_2d/* OpenPCDet/pcdet/models/backbones_2d/
+
+cp -r src/models/backbones_3d/*.py OpenPCDet/pcdet/models/backbones_3d/
+cp -r src/models/backbones_3d/pfe/* OpenPCDet/pcdet/models/backbones_3d/pfe/
+cp -r src/models/backbones_3d/cfe OpenPCDet/pcdet/models/backbones_3d/
+
+cp -r src/models/detectors/* OpenPCDet/pcdet/models/detectors/
+
+cp -r src/ops/pointnet2/pointnet2_stack/* OpenPCDet/pcdet/ops/pointnet2/pointnet2_stack/
+cp -r src/ops/pointnet2/pointnet2_batch/* OpenPCDet/pcdet/ops/pointnet2/pointnet2_batch/
+
+cp -r src/tools/* OpenPCDet/tools/
+
+cp -r configs/* OpenPCDet/tools/cfgs/kitti_models/
+
