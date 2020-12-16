3668e6c87a4e39f41aa88d181875e8f42fd980ae
Prarthana <p6bhatta@uwaterloo.ca>
2020-12-16 05:07:52 -0500

    Add fsa and dsa in pfe

---
+++ src/models/backbones_3d/pfe/__init__.py
@@ -0,0 +1,8 @@
+from .voxel_set_abstraction import VoxelSetAbstraction
+from .sa_voxel_set_abstraction import SAVoxelSetAbstraction
+from .def_voxel_set_abstraction import DefVoxelSetAbstraction
+__all__ = {
+    'VoxelSetAbstraction': VoxelSetAbstraction,
+    'DefVoxelSetAbstraction': DefVoxelSetAbstraction,
+    'SAVoxelSetAbstraction': SAVoxelSetAbstraction,
+}
